42c2b6076e92d749bc89bd4ebf2558219f57ed08
J M <jm@Js-MacBook-Air.local>
2024-08-31 13:07:29 +0300

    java script student data

---
+++ jibril.js
@@ -0,0 +1,29 @@
+const studentData = () => {
+
+    return [
+
+        {
+            ID: 1,
+            name: "Jibril Abdi", 
+            age: 18,
+            gpa: 4.2
+        },
+        {
+            ID: 2,
+            name: "Mohamed Abdi", 
+            age: 20,
+            gpa: 4.1,
+        },
+        {
+            ID: 3,
+            name: "Abdullahi Abdi", 
+            age: 13,
+            gpa: 4.2,
+        }
+
+        
+    ];
+}
+
+studentData();
+
