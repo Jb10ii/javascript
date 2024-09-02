80f8a954970a070a48e2c9cc88b517fa38cc76bf
J M <jm@Js-MacBook-Air.local>
2024-09-02 13:09:24 +0300

    new java script grading system code

--- jibril.js
+++ jibril.js
@@ -1,29 +1,13 @@
-const studentData = () => {
-
-    return [
-
-        {
-            ID: 1,
-            name: "Jibril Abdi", 
-            age: 18,
-            gpa: 4.2
-        },
-        {
-            ID: 2,
-            name: "Mohamed Abdi", 
-            age: 20,
-            gpa: 4.1,
-        },
-        {
-            ID: 3,
-            name: "Abdullahi Abdi", 
-            age: 13,
-            gpa: 4.2,
-        }
-
-        
-    ];
+const studentGrade = parseInt(prompt("What is your Students Grade")) 
+if ((studentGrade >=0) && (studentGrade <50)) {
+   console.log ("You have failed and your grade is a F")
+} else if ((studentGrade >=50) && (studentGrade <70)) {
+    console.log ("You have a average grade and your grade is a C")
+} else if ((studentGrade >=70) && (studentGrade <90)) {
+    console.log ("You have a above average grade and your grade is a B")
+} else if ((studentGrade >=90) && (studentGrade <101)) {
+    console.log ("you have a excellent grade and your grade is a A")
 }
-
-studentData();
-
+ else {
+    console.log ("Invalid Score");
+};
